1c9110df951c2e1b00ac78bbd87ed447bf94a211
matheusnajal <matheusnajalk@gmail.com>
2024-02-15 14:21:51 -0300

    15/02

--- Programação_Funcional/Listas/Programação_Condicional/Ex1037.clj
+++
@@ -1,9 +0,0 @@
-(defn intervalo [a]
-  (cond 
-    (and (>= a 0) (<= a 25))  (println "Intervalo [0,25]")
-    (and (> a 25) (<= a 50)) (println "Intervalo (25,50]")
-    (and (> a 50) (<= a 75)) (println "Intervalo (50,75]")
-    (and (> a 75) (<= a 100)) (println "Intervalo (75,100]")
-    :else (println "Fora de intervalo")))
-
-(intervalo (read))
