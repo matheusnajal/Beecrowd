f5185599e9f52a66db2ad25f2a31eb8dd0f62a90
matheusnajal <matheusnajalk@gmail.com>
2024-03-15 19:57:46 -0300

    fix

--- Estrutura_Sequencial/Java/Ex1002/Main.java
+++ Estrutura_Sequencial/Java/Ex1002/Main.java
@@ -3,6 +3,7 @@ package Ex1002;
 import java.util.Scanner;
 
 public class Main {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double raio = sc.nextDouble();
