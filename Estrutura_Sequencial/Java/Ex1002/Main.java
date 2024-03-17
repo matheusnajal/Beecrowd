15fadbcd2f06abb26b6e9b3520099fbe72b50525
matheusnajal <matheusnajalk@gmail.com>
2024-03-17 18:53:38 -0300

    aff ta em java

--- Estrutura_Sequencial/Java/Ex1002/Main.java
+++ Estrutura_Sequencial/Java/Ex1002/Main.java
@@ -3,7 +3,6 @@ package Ex1002;
 import java.util.Scanner;
 
 public class Main {
-    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double raio = sc.nextDouble();
@@ -11,5 +10,6 @@ public class Main {
         double area = n * Math.pow(raio, 2);
 
         System.out.printf("A=%.4f\n", area);
+        sc.close();
     }
 }
